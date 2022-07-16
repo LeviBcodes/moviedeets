98c6b815ee5b48f929f90c74cc2dd98651e9586e
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2022-07-16 14:21:46 -0600

    itterating through nested arrays in returned data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useCallback} from 'react';
 import useMediaSearch from './useMediaSearch';
 
 function App() {
   const[query, setQuery] = useState('');
   const[pageNumber, setPageNumber] = useState(1);
-  useMediaSearch(query, pageNumber);
+  
+  const {
+    loading,
+    error,
+    media,
+    hasMore
+  } = useMediaSearch(query, pageNumber)
+  
+  const observer = useRef()
+  
+  const lastMediaElementRef = useCallback(node => {
+    if(loading) return
+    if(observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    }, {threshold: 0.5})
+    if(node) observer.current.observe(node)
+  }, [])
+  
+  function handleSearch(e) {
+    e.preventDefault();
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+  
   return (
     <>
-      <input type="text" />
+      <input type="text" value={query} onChange={handleSearch} className="" />
+      {media && media.map((item, index) => {
+        item.Search && item.Search.map((value, key) => {
+          console.log(value.Title)
+      })})}
     </>
   );
 }
--- src/useMediaSearch.js
+++ src/useMediaSearch.js
@@ -1,21 +1,39 @@
 import {useEffect, useState} from 'react'
 import axios from 'axios';
+import { logDOM } from '@testing-library/react';
 
 export default function useMediaSearch(query, pageNumber) {
+    const[loading, setLoading] = useState(false)
+    const[error,setError] = useState(false)
+    const[media, setMedia] = useState([])
+    const[hasMore, setHasMore] = useState(false)
+
+    const api = "http://www.omdbapi.com/?apikey=a619790&plot=full";
 
     useEffect(() => {
-        axios({
-            method: 'GET',
-            url: 'http://www.omdbapi.com/?apikey=a619790',
-            params: {
-                s: query,
-                page: pageNumber
-            }
-        }).then(res => {
-            console.log(res.data)
-            }).catch(err => {
-                console.log(err)
+        setMedia([])
+    }, [query])
+
+    useEffect(() => {
+        setLoading(true)
+        setError(false)
+        let cancel
+        axios.get(api, {
+            params: {s: query, page: pageNumber},
+            cancelToken: new axios.CancelToken(c => cancel = c)
+            },
+            )
+            .then(res => {
+                setMedia(prevMedia => {
+                    return [...new Set([...prevMedia, ...[res.data]])]
+                })
+                setHasMore(res.data.totalResults > 0)
+                setLoading(false)
+            }).catch(e => {
+                if(axios.isCancel(e)) return
+                setError(true)
             })
+        return () => cancel()
     }, [query, pageNumber]);
-  return null
+  return {loading, error, media, hasMore}
 }
