128e33c09001ac6aa636a3e037c1282b0d72253e
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2022-07-20 17:54:25 -0600

    adding content for about website page

--- src/About.js
+++ src/About.js
@@ -6,8 +6,8 @@ const About = () => {
     return (
         <>
             <div id="about" className="z-10 mx-auto bottom-0 top-0 box-border fixed left-0 right-0 bg-black bg-opacity-95 rounded-md transition-all ease-linear duration-400 text-white overflow-auto" >
-                <h1 className="p-20 text-3xl font-bold">About this site</h1>
-                <p>Hi, I'm Levi Burland. I created this web application using the following technologies:</p>
+                <h1 className="pt-40 text-3xl font-bold">About this site</h1>
+                <p className=" ">Hi, I'm Levi Burland. I created this web application using the following technologies:</p>
                 <ul>
                     <li>React</li>
                     <li>Axios</li>
